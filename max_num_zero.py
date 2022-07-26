600073890c21e1d8140470141b46d10c750cecfe
Muhammad Adnan Anwar <67798646+adnan-math@users.noreply.github.com>
2022-07-26 15:29:19 +0500

    with comments

--- max_num_zero.py
+++ max_num_zero.py
@@ -1,9 +1,12 @@
  def solution(N):
     x=format(N, 'b')
+   #format function takes a number and convert it into binary, you can also use bin() dunction
     res = [int(u) for u in str(x)]  
+    # convert a integer into list
     index=[]  
     c=[]
     index=[i for i, e in enumerate(res) if e == 1]
+    #ind the index/address for each 1's in the list
     for i in range(len(index)):
         if index[i]-index[i-1] >1:
             c.append(index[i]-index[i-1]-1)
